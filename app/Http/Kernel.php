49e586381ca2a35997e8db4d82c78216ff724706
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2025-11-20 17:31:09 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\ForceHttpsMiddleware::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\ForceHttps::class,s,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
